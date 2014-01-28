aec8ec9b973bb6a12263f667c60b2d23d2ec1c2b
nanjotah <nanjotah@gmail.com>
2014-01-28 19:21:37 +0100

    comments modified

--- twitterapi.py
+++ twitterapi.py
@@ -5,18 +5,20 @@
 
 import tweepy
 
-
 class twitter:
     
     def __init__(self):
         """        
-        Initialisation de la classe, ne requiert aucun parametre (hard coded authentification)
+        Class initialisation - hard coded authentification
         """
         self.auth_handler = tweepy.OAuthHandler("c9i3PW0gezoXzBuJVgqzZQ","veqwSzAPniNFQyHCtuCAb3ciqj0CbAw4Lef0BfmHoUM")
         self.auth_handler.set_access_token("2315490229-TwWACB6UUaqv8odiw7lwnVi5d8EnjUu8ADrFg9n","SoPToYmmtswjIE3UFE64NG1aXgm4gU36SMMB1fpofaIvN")
         self.twitter_api = tweepy.API(self.auth_handler)
     
     def search(self, hashtag_list):
+        """
+        Twitter API Search function implementation for this usage
+        """
         tweets_list = []
         if hashtag_list == [] or hashtag_list == "" or isinstance(hashtag_list,str) != True:
             print "Search field empty"
